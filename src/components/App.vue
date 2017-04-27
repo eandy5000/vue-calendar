8273a2f371d53521028095ab4f123bfaf177b37b
Andy Eccleston <eandy5000@yahoo.com>
2017-04-27 10:58:14 -0500

    added calendar month logic with padding to App component

--- src/components/App.vue
+++ src/components/App.vue
@@ -10,23 +10,44 @@
         data() {
             return {
                 year: 2017,
-                month: 4
+                month: 5
             }
         },
         computed: {
             days() {
+                const SUNDAY = 0;
+                const MONDAY = 1;
+
                 let days = [];
                 let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
                 do {
                     days.push(currentDay);
                     currentDay = this.$moment(currentDay).add(1, 'days');
                 } while ((currentDay.month() + 1) === this.month);
+
+                currentDay = this.$moment(days[0]);
+
+                if(currentDay.day() !== MONDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).subtract(1, 'days');
+                        days.unshift(currentDay)
+                    } while (currentDay.day() !== MONDAY);
+                }
+
+
+                currentDay = this.$moment(days[days.length - 1]);
+
+                if (currentDay.day() !== SUNDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).add(1, 'days');
+                        days.push(currentDay)
+                    } while (currentDay.day() !== SUNDAY);
+                }
+
+        
                 return days;
                
             }
-        },
-        created(){
-            console.log(this.$moment)
         }
     }
 </script>
